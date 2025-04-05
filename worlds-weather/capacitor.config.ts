13ef6f6b96527b5b3ad6c6442920bc1f00db8fc4
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-04-05 11:57:18 +0800

    deleted

--- worlds-weather/capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'worlds-weather',
-  webDir: 'www'
-};
-
-export default config;
