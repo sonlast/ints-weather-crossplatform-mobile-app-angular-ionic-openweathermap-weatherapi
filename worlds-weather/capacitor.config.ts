3fd34a1c883484dfa98ecb02c224cea464f75324
Ijerson Lastimosa Ilalto <77521841+sonlast@users.noreply.github.com>
2025-04-06 19:30:11 +0800

    added new project folder

---
+++ worlds-weather/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'worlds-weather',
+  webDir: 'www'
+};
+
+export default config;
